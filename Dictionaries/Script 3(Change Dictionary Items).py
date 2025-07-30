1364c90370bd01cf1e5259b7ffc2fdc76666e6e2
Muskan Mittal <106099938+Muskan29mm@users.noreply.github.com>
2025-07-30 18:10:26 +0530

    Add files via upload

---
+++ Dictionaries/Script 3(Change Dictionary Items).py
@@ -0,0 +1,21 @@
+# Change Dictionary Items
+
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1976
+}
+print(thisdict)
+thisdict["year"] = 1985
+print(thisdict)
+
+# Update dictionary
+
+dict1 = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+print(dict1)
+dict1.update({"year": 2000})
+print(dict1)
